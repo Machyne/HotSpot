fb266fe04e9e8cf4b4c0665a7b812b4f29972126
Machyne <mcotter222@gmail.com>
2013-11-02 00:55:33 -0500

    nothing

--- main.js
+++ main.js
@@ -12,6 +12,7 @@ if (Meteor.isClient) {
     }
   });
   Meteor.startup( function () {
+
     Maps.init();
     Spots.find().forEach( function(doc, index, cursor) {
       Maps.addPoint({lat: doc.lat, lng: doc.lng, name: doc.name, id: doc._id}, true); //noUpdate = true
@@ -23,5 +24,6 @@ if (Meteor.isClient) {
 if (Meteor.isServer) {
   Meteor.startup(function () {
     // code to run on server at startup
+    // Spots.remove({});
   });
 }
